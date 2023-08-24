869c8638c3fd494aa320ccd5cc689c6168432487
Paulina <paulinagpyl@gmail.com>
2023-08-24 11:23:47 -0400

    con menu de 4 opciones

--- calc.js
+++ calc.js
@@ -10,11 +10,24 @@ var n1, n2, suma;
 
 seguir = prompt("Bienvenido a la calculadora, ¿quiere seguir? s/n: ");
 if (seguir == "s") {
-    nombre = prompt("Ingrese nombre: ");
-    n1 = parseInt(prompt("Ingrese número 1: "));
-    n2 = parseFloat(prompt("Ingrese número 2: "));
-    suma = n1 + n2;
-    console.log(suma);
+    opcion = parseInt(prompt("1.- Suma, 2.- Resta, 3.- Multiplicación, 4.- División, ¿quiere hacer?:"));
+    if (opcion ==1){
+        console.log ("esto es suma")
+        n1 = parseInt(prompt("Ingrese número 1: "));
+        n2 = parseFloat(prompt("Ingrese número 2: "));
+        suma = n1 + n2;
+        console.log(suma)
+    }
+    else if (opcion ==2)
+    {
+        console.log ("esto es resta")
+    }
+    else if (opcion ==3){
+        console.log ("esto es multiplicacion")
+    }
+    else {
+        console.log ("esto es division")
+    }
 }
 else
     console.log("Chau");
